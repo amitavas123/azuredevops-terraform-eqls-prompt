6c703327195739ae130f6c03df1a939ca50b2cf3
amitavas123 <amitavas123@gmail.com>
2026-06-25 22:37:41 +1000

    Stages fixed10

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -21,7 +21,7 @@ variable "environment" {
 variable "location" {
   description = "Azure region for resources"
   type        = string
-  default     = "East US"
+  default     = "Australia East"
 }
 
 variable "tags" {
